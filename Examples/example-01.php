dccd9806af363aa0aa577c5ef81601a41e282fba
alessandro <alessandro.manno@facile.it>
2016-12-29 14:10:26 +0100

    typo

--- Examples/example-01.php
+++ Examples/example-01.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Compos
 use Formaggio\Formaggio;
 
 $formaggio = new Formaggio('/users', 'post');
-$formaggio->text('name', ["value" => "alessandro"])->place('Alessandro');
+$formaggio->text('name', ["value" => "Alessandro"])->place('Alessandro');
 $formaggio->text('lastname')->place('Manno');
 $formaggio->email('email')->place('alessandromanno@gmail.com');
 $formaggio->password('password');
